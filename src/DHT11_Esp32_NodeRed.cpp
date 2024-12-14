c7aeb31c035cb635623025ab7bd3d6690364ae94
EARTHISALWAYSHAPPY <eathdis@gmail.com>
2024-12-15 02:43:26 +0700

    commit

--- src/DHT11_Esp32_NodeRed.cpp
+++ src/DHT11_Esp32_NodeRed.cpp
@@ -1,20 +1,49 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
 #include <DHT11.h>
-DHT11 dht11(5);
-void setup() {
-    Serial.begin(9600);
+
+#define WIFI_SSID "Pee Kub_2.4G"
+#define WIFI_PASSWORD "Pee1542547"
+
+#define MQTT_HOST "--------------"
+#define MQTT_PORT 1883
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+#define DHTPIN 5
+DHT11 dht11(DHTPIN);
+
+float temperature;
+u_int8_t humhumidity;
+void setup()
+{
+    Serial.begin(112500);
+    //WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(100);
+        Serial.print(".");
+    }
+    Serial.println("Connected Done!!!");
+    delay(2000);
 }
 
-void loop() {
+void loop()
+{
     int temperature = 0;
     int humidity = 0;
     int result = dht11.readTemperatureHumidity(temperature, humidity);
-    if (result == 0) {
+    if (result == 0)
+    {
         Serial.print("Temperature: ");
         Serial.print(temperature);
         Serial.print(" °C\tHumidity: ");
         Serial.print(humidity);
         Serial.println(" %");
-    } else {
+    }
+    else
+    {
         Serial.println(DHT11::getErrorString(result));
     }
 }
