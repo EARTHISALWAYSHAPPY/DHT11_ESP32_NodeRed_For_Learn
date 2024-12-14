1b46551dce0692665b6ff70ee892918c193d96be
EARTHISALWAYSHAPPY <eathdis@gmail.com>
2024-12-15 03:24:10 +0700

    update

--- src/DHT11_Esp32_NodeRed.cpp
+++ src/DHT11_Esp32_NodeRed.cpp
@@ -1,6 +1,8 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
-#include <DHT11.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
 #define WIFI_SSID "Pee Kub_2.4G"
 #define WIFI_PASSWORD "Pee1542547"
@@ -11,39 +13,42 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 #define DHTPIN 5
-DHT11 dht11(DHTPIN);
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
-float temperature;
-u_int8_t humhumidity;
+uint32_t delayMS;
+
 void setup()
 {
-    Serial.begin(112500);
-    //WiFi.mode(WIFI_STA);
+    Serial.begin(115200);
+    dht.begin();
+    // WiFi.mode(WIFI_STA);
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     while (WiFi.status() != WL_CONNECTED)
     {
         delay(100);
-        Serial.print(".");
+        Serial.print(F("."));
     }
-    Serial.println("Connected Done!!!");
+    Serial.println(F("Connected Done!!!"));
     delay(2000);
 }
 
 void loop()
-{
-    int temperature = 0;
-    int humidity = 0;
-    int result = dht11.readTemperatureHumidity(temperature, humidity);
-    if (result == 0)
-    {
-        Serial.print("Temperature: ");
-        Serial.print(temperature);
-        Serial.print(" °C\tHumidity: ");
-        Serial.print(humidity);
-        Serial.println(" %");
-    }
-    else
-    {
-        Serial.println(DHT11::getErrorString(result));
-    }
+{   
+  delay(1000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.println(F("°F"));
 }
